57af236c09fe41f71d2fa25602e568b51c89bef0
13903590580 <644460347@qq.com>
2019-08-25 08:50:50 +0800

    'lesson-five'

---
+++ src/store/modules/count.js
@@ -0,0 +1,31 @@
+const state = {
+    count: 0
+};
+const getters = {
+    count: state => state.count
+    // count(state) {
+    //   return ++state.count
+    // },
+};
+const mutations = {
+    incrementCount: state => state.count++,
+    decrementCount: (state, payload) => state.count -= payload.amount
+};
+const actions = {
+    incrementCountAsync: ({ commit }) => {
+        setTimeout(() => {
+            // context./** = this.$store */
+            commit("incrementCount")
+        }, 1000);
+    },
+    decrementCountAsync: (context, payload) => {
+        setTimeout(() => {
+            // context./** = this.$store */
+            commit("decrementCount", payload)
+        }, 2000);
+    }
+};
+
+export default {
+    state, getters, mutations, actions
+}
